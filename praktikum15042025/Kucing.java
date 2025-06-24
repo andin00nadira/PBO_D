a7fb40fd6e293229ea59fb7e19128da4a3221c4c
Nadira putri rahayu <nadiraputri1501qb@gmail.com>
2025-06-24 12:29:03 +0700

    Add files via upload

---
+++ praktikum15042025/Kucing.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum15042025;
+
+/**
+ *
+ * @author Ari
+ */
+public class Kucing extends Hewan{
+    public Kucing(String nama){
+        super(nama);
+    }
+    
+    @Override
+    public void Suara(){
+        System.out.println(nama+" bersuara : Meong!!!");
+    }
+}
